70f03001f5781dc96290ba2678126267014a9776
preetikaGLA <31967910+preetikaGLA@users.noreply.github.com>
2017-09-14 23:48:43 +0530

    generator( ) added

--- advancedTopics.py
+++ advancedTopics.py
@@ -1,3 +1,11 @@
 #list comprehension in python
 a=[x**2 for x in range(1,5)]
 print(a)
+
+#generator() function added
+def gen():
+  for x in range(5,11):
+    yeald x**2
+a=gen()
+print('print generator object',a)
+print('next value',next(a))
