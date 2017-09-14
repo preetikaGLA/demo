c187dd512591150abb9622d6810626d1ff5260da
preetikaGLA <31967910+preetikaGLA@users.noreply.github.com>
2017-09-14 23:51:55 +0530

    generator expression added

--- advancedTopics.py
+++ advancedTopics.py
@@ -1,3 +1,8 @@
 #list comprehension in python
 a=[x**2 for x in range(1,5)]
 print(a)
+
+#generator expression
+b=(x**2 for x in range(5,11))
+print('generator object',b)
+print('next value',next(b))
